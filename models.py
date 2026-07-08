5e0ddfdadbc7a289715182713bb1b8f05f73856b
tangocphuc <phucdihoc133@gmail.com>
2026-07-09 03:50:23 +0700

    Add models.py

---
+++ models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+
+class Student(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True, index=True)
+    full_name = Column(String(100), nullable=False)
+    email = Column(String(100), nullable=False)
